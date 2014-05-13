20f58a57f04739ee8644341f9f877daebf140115
Sameer Naik <sameer@damagehead.com>
2014-05-13 21:33:16 +0530

    added smtp_settings.rb smtp configuration template in config directory

---
+++ assets/config/smtp_settings.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "{{SMTP_HOST}}",
+    :port                 => {{SMTP_PORT}},
+    :domain               => "{{SMTP_DOMAIN}}",
+    :user_name            => "{{SMTP_USER}}",
+    :password             => "{{SMTP_PASS}}",
+    :authentication       => :login,
+    :enable_starttls_auto => {{SMTP_STARTTLS}}
+  }
+end
