e6bcff9c1c7250528302a754adbae68d210ae4d3
Alexey Bednyakov <x@rxtx.wtf>
2016-10-21 00:49:55 +0300

    Comment fix
    Updated from GITLAB_USER to REDMINE_USER

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -6,7 +6,7 @@ GEM_CACHE_DIR="${REDMINE_BUILD_DIR}/cache"
 BUILD_DEPENDENCIES="libcurl4-openssl-dev libssl-dev libmagickcore-dev libmagickwand-dev \
                     libmysqlclient-dev libpq-dev libxslt1-dev libffi-dev libyaml-dev"
 
-## Execute a command as GITLAB_USER
+## Execute a command as REDMINE_USER
 exec_as_redmine() {
   sudo -HEu ${REDMINE_USER} "$@"
 }
