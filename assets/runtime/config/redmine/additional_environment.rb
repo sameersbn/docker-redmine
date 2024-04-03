10381838b2a4ad2e6ea864eb24f3856f3650d321
siberianlove <siberian.love.mail+github@gmail.com>
2024-04-03 12:58:24 +0500

    Add logger level configuration support

--- assets/runtime/config/redmine/additional_environment.rb
+++ assets/runtime/config/redmine/additional_environment.rb
@@ -1,4 +1,10 @@
-config.gem 'dalli'
-config.action_controller.perform_caching  = true
-config.cache_classes = true
-config.cache_store = :mem_cache_store, "{{MEMCACHE_HOST}}:{{MEMCACHE_PORT}}"
+# Copy this file to additional_environment.rb and add any statements
+# that need to be passed to the Rails::Initializer.  `config` is
+# available in this context.
+#
+# Example:
+#
+#   config.log_level = :debug
+#   ...
+#
+
